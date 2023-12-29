470e555dec39cd46afa56ca7ddc62defba7a7f1e
Platform Jess <caljess599@gmail.com>
2023-12-28 20:21:03 -0500

    Refactor for multiple lookup
    Solve problem of asynchronous lookups of multiple addresses

--- app/app.js
+++ app/app.js
@@ -1,16 +1,17 @@
 var formEl = document.getElementById("form");
+var displayTable = document.getElementById("resultsTable");
+var showClearButton = document.getElementById("clearBtn");
 
 document.getElementById("clearBtn").addEventListener("click", function () {
-  var showResults = document.getElementById("results");
-  var showAddress = document.getElementById("fullAddress");
-  var showPrecinct = document.getElementById("precintName");
-
-  showResults.innerText = "";
-  showAddress.innerText = "";
-  showPrecinct.innerText = "";
+  // hide the table
+  showClearButton.hidden = true;
+  displayTable.hidden = true;
 });
 
 formEl.addEventListener("submit", function (event) {
+  var placeholder = document.getElementById("data-output");
+
+  var output = "";
   var formData = new FormData(event.target);
   var address = {};
   formData.forEach(function (value, key) {
@@ -20,66 +21,75 @@ formEl.addEventListener("submit", function (event) {
   console.log("Address data", address);
 
   var street_num = address.street_num;
-  var street_name = address.street_name;
 
-  // var url = "https://gisweb.charlottesville.org/cvgisweb/rest/services/OpenData_1/MapServer/76/query?where=StreetNumber%20%3D%20'211'%20AND%20StreetName%20%3D%20'ALDERMAN'&outFields=FullAddress,ParcelIDNumber&outSR=4326&f=json";
-  var url = `https://gisweb.charlottesville.org/cvgisweb/rest/services/OpenData_1/MapServer/76/query?where=StreetNumber%20%3D%20'${street_num}'%20AND%20StreetName%20%3D%20'${street_name.toUpperCase()}'&outFields=FullAddress,ParcelIDNumber&outSR=4326&f=json`;
+  // alert if street number is blank
 
-  var responseAddressLookup = fetch(url);
-  var showResults = document.getElementById("results");
-  var showAddress = document.getElementById("fullAddress");
-  var showPrecinct = document.getElementById("precintName");
-  var showClearButton = document.getElementById("clearBtn");
+  // we need to handle 9 1/2 (and probably a lot of other exceptions, too)
+  var street_name = address.street_name.toUpperCase();
+  //var street_name = address.street_name.toUpperCase().split(" ")[0];
 
-  // first api call
-  responseAddressLookup // this is the fetch response
-    .then((response) => response.json()) // "response" is arbitrary name for responsePromise; .json() returns a promise that resolves with a JSON Object
-    .then((data) => {
-      // calling response.json() data and doing things with it
-      console.log("Address request succeeded!");
-      var features = data.features[0];
-      var fullAddress = features.attributes.FullAddress;
-      var geox = features.geometry.x;
-      var geoy = features.geometry.y;
-      console.log(fullAddress);
-      console.log(geox);
-      console.log(geoy);
+  // first API call; return JSON object
+  const urlOuter = `https://gisweb.charlottesville.org/cvgisweb/rest/services/OpenData_1/MapServer/76/query?where=StreetName%20%3D%20'${street_name}'%20AND%20StreetNumber%20%3D%20'${street_num}'&outFields=FullAddress&outSR=4326&f=json`;
 
-      // if else should go here
+  let urlInner = "";
 
-      var url2 = `https://gisweb.charlottesville.org/arcgis/rest/services/OpenData_1/MapServer/12/query?where=1%3D1&outFields=PrecinctName,PrecinctNumber&geometry=${geox},${geoy},${geox},${geoy}&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&returnGeometry=false&outSR=4326&f=json`;
-
-      //second api call
-      var responsePrecinctLookup = fetch(url2);
-
-      responsePrecinctLookup // this is the fetch response
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("request succeeded with JSON response");
-          var features = data.features[0];
-          var precinctName = features.attributes.PrecinctName;
-          var precinctNumber = features.attributes.PrecinctNumber;
-
-          showResults.innerText = "Found address:";
-          showAddress.innerText = fullAddress;
-          showPrecinct.innerText = `${precinctName} (${precinctNumber})`;
-          showClearButton.hidden = false;
-
-          // handle error from responsePrecinctLookup
-        })
-        .catch(function (error) {
-          showResults.innerText =
-            "request failed, can't find precinct for address";
-          console.log("request failed", error);
-        });
-
-      // handle error from responseAddressLookup
+  const firstPromise = fetch(urlOuter)
+    .then((responseO) => responseO.json())
+    .then((responseBodyO) => {
+      return responseBodyO.features;
     })
-    .catch(function (error) {
-      showResults.innerText = "request failed, try another address";
-      console.log("request failed", error);
+    .catch((err) => {
+      console.error("Failed to fetch - " + urlOuter);
+      console.error(err);
     });
 
+  firstPromise.then((addresses) => {
+    console.log("First promise:");
+    console.log(addresses);
+    let precinctLookups = addresses.map((address) => {
+      // var address = addresses[0]
+      var fullAddress = address.attributes.FullAddress;
+      var geometry = address.geometry;
+      var geox = geometry.x;
+      var geoy = geometry.y;
+      urlInner = `https://gisweb.charlottesville.org/arcgis/rest/services/OpenData_1/MapServer/12/query?where=1%3D1&outFields=PrecinctName,PrecinctNumber&geometry=${geox},${geoy},${geox},${geoy}&geometryType=esriGeometryEnvelope&inSR=4326&spatialRel=esriSpatialRelIntersects&returnGeometry=false&outSR=4326&f=json`;
+      return fetch(urlInner)
+        .then((responseI) => responseI.json())
+        .then((responseBodyI) => {
+          console.log("Making inner request:");
+          var precinctDetails = responseBodyI.features[0];
+          var precinctName = precinctDetails.attributes.PrecinctName;
+          var precinctNumber = precinctDetails.attributes.PrecinctNumber;
+          var precinct = `${precinctName} (${precinctNumber})`;
+          address.attributes.Precinct = precinct;
+          console.log("I'm going to return:");
+          console.log(address);
+          return address;
+        })
+        .catch((err) => {
+          console.error("Failed to fetch - " + urlInner);
+          console.error(err);
+        });
+    }); // end map loop
+    Promise.all(precinctLookups).then((addresses) => {
+      // All the 'return' operations will be executed when the .then is executed
+      console.log("All the promises have been executed here:");
+      console.log(addresses);
+      let outputLoop = addresses.map((address) => {
+        output += `
+          <tr>
+            <td> ${address.attributes.FullAddress}</td>
+            <td> ${address.attributes.Precinct}<td>
+          </tr>`;
+      });
+
+      displayTable.hidden = false;
+      placeholder.innerHTML = output;
+      showClearButton.hidden = false;
+      return outputLoop;
+    });
+    console.log("Output out here");
+  });
   formEl.reset();
   event.preventDefault();
 });
