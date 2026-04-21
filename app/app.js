1d041dc10befc33e7b5d0b6e1af835061b371f64
Platform Jess <caljess599@gmail.com>
2026-04-20 22:04:27 -0400

    Update app.js
    Changed two more precinct names (Buford and Johnson)

--- app/app.js
+++ app/app.js
@@ -10,8 +10,8 @@ var summitAddress = "https://www.google.ca/maps/place/Summit+Elementary+School+(
 var carverAddress = "https://www.google.ca/maps/place/Carver+Recreation+Center/@38.0322512,-78.4868992,17z/data=!3m1!5s0x89b3863acc2505bf:0x945732ebb2d10b75!4m15!1m8!3m7!1s0x89b3863ace9cbb4d:0xbeea6151ebcf4132!2s233+4th+St+NW,+Charlottesville,+VA+22903!3b1!8m2!3d38.0322512!4d-78.4868992!16s%2Fg%2F11b8v4m6pj!3m5!1s0x89b386254655169b:0xe9ad5aa72fb34202!8m2!3d38.0321537!4d-78.487004!16s%2Fg%2F1tmqs7h1"
 var trailblazerAddress = "https://www.google.ca/maps/place/Trailblazer+Elementary+School+(formerly+Venable)/@38.0385172,-78.4965487,17z/data=!4m15!1m8!3m7!1s0x89b3863638a5d70b:0x8bc8e30d018e6323!2s406+14th+St+NW,+Charlottesville,+VA+22903!3b1!8m2!3d38.0385326!4d-78.4965365!16s%2Fg%2F11c43tz15r!3m5!1s0x89b3863638d7ad43:0x258916bd1038a411!8m2!3d38.0385402!4d-78.4966733!16s%2Fm%2F0766mrv"
 var jacksonViaAddress = "https://www.google.ca/maps/place/Jackson+Via+Elementary+School/@38.014141,-78.5038972,17z/data=!3m1!4b1!4m6!3m5!1s0x89b3867196f1e3df:0x83890429d3119ca7!8m2!3d38.014141!4d-78.5038972!16s%2Fm%2F076d7bk"
-var johnsonAddress = "https://www.google.ca/maps/place/Johnson+Elementary+School/@38.0218941,-78.5062384,17z/data=!4m15!1m8!3m7!1s0x89b386691ec889a9:0x977c62f3d494ff5b!2s1645+Cherry+Ave,+Charlottesville,+VA+22903!3b1!8m2!3d38.0218941!4d-78.5062384!16s%2Fg%2F11c43zqh30!3m5!1s0x89b386691d775a35:0x268fbe8adc3becb3!8m2!3d38.021929!4d-78.506274!16s%2Fm%2F0767syd"
-var bufordAddress = "https://www.google.ca/maps/place/Buford+Middle+School/@38.0270168,-78.497917,17z/data=!4m15!1m8!3m7!1s0x89b3863f78bd4d33:0x6d2b786e4b58f3c!2s1000+Cherry+Ave,+Charlottesville,+VA+22903!3b1!8m2!3d38.0270168!4d-78.497917!16s%2Fg%2F11c43ymbp0!3m5!1s0x89b3863f87c61035:0x61367e79b57db7bc!8m2!3d38.0264069!4d-78.4969929!16s%2Fm%2F03cpjz8"
+var talloaksAddress = "https://www.google.ca/maps/place/Johnson+Elementary+School/@38.0218941,-78.5062384,17z/data=!4m15!1m8!3m7!1s0x89b386691ec889a9:0x977c62f3d494ff5b!2s1645+Cherry+Ave,+Charlottesville,+VA+22903!3b1!8m2!3d38.0218941!4d-78.5062384!16s%2Fg%2F11c43zqh30!3m5!1s0x89b386691d775a35:0x268fbe8adc3becb3!8m2!3d38.021929!4d-78.506274!16s%2Fm%2F0767syd"
+var cvillemiddleschoolAddress = "https://www.google.ca/maps/place/Charlottesville+Middle+School/@38.0263308,-78.5157362,15z/data=!4m10!1m2!2m1!1scharlottesville+middle+school!3m6!1s0x89b3872cb3f036d3:0xf43ae619cdb54767!8m2!3d38.0263308!4d-78.4966818!15sCh1jaGFybG90dGVzdmlsbGUgbWlkZGxlIHNjaG9vbJIBDW1pZGRsZV9zY2hvb2zgAQA!16s%2Fg%2F11xsf5ffnp?entry=ttu&g_ep=EgoyMDI2MDQxNS4wIKXMDSoASAFQAw%3D%3D"
 var chsAddress = "https://www.google.ca/maps/place/Charlottesville+High+School/@38.0516655,-78.4746367,17z/data=!4m15!1m8!3m7!1s0x89b388787a8c0d7d:0x362604d7f8b76c1!2s1400+Melbourne+Rd,+Charlottesville,+VA+22901!3b1!8m2!3d38.0516655!4d-78.4746367!16s%2Fg%2F11jl_gxg76!3m5!1s0x89b387d7966dfff1:0xfffb3e9192a14eb!8m2!3d38.0528624!4d-78.4753177!16zL20vMGMxaDQ5"
 var walkerAddress = "https://www.google.ca/maps/place/1699+Rose+Hill+Dr,+Charlottesville,+VA+22903/@38.0528895,-78.4859052,16z/data=!3m1!4b1!4m6!3m5!1s0x89b387d08da3aff3:0x1e9233413f22579b!8m2!3d38.0528853!4d-78.4833303!16s%2Fg%2F11cncgf8bh"
 
@@ -82,11 +82,11 @@ formEl.addEventListener("submit", function(event) {
               case "Jackson-Via":
                 precinctLinkedName = `Jackson-Via Precinct (<a href="${jacksonViaAddress}" target="_blank">Google map</a>)</br>Jackson-Via Elementary School`
                 break;
-              case "Johnson":
-                precinctLinkedName = `Johnson Precinct (<a href="${johnsonAddress}" target="_blank">Google map</a>)</br>Johnson Elementary School Cafeteria`
+              case "Tall Oaks/Johnson":
+                precinctLinkedName = `Tall Oaks Precinct (<a href="${talloaksAddress}" target="_blank">Google map</a>)</br>Johnson Elementary School Cafeteria`
                 break;
-              case "Buford":
-                precinctLinkedName = `Buford Precinct (<a href="${bufordAddress}" target="_blank">Google map</a>)</br>Buford Middle School Media Center`
+              case "CMS/Buford":
+                precinctLinkedName = `Charlottesville Middle School Precinct (<a href="${cvillemiddleschoolAddress}" target="_blank">Google map</a>)</br>Buford Middle School Media Center`
                 break;
               case "CHS":
                 precinctLinkedName = `Charlottesville High Precinct (<a href="${chsAddress}" target="_blank">Google map</a>)</br>Charlottesville High School Cafeteria`
